e7ab5d9dd25ec248043c9be64c43923dcc05ab9c
ajaybiswas22 <ajaybiswas22@gmail.com>
2020-03-05 01:59:47 +0530

    Add files via upload

---
+++ SourceCode/userlinking/questionpapers_in.php
@@ -0,0 +1,8 @@
+<?php
+$linkback = "../ComputerScience/computerscience.php";
+$linknext = "../ComputerScience/practice.php";
+
+$link_array = array("../ComputerScience/questionpapers.php","../ComputerScience/computerscience.php","../ComputerScience/practice.php");
+$linktext_array = array("Question Papers","Computer Science","Practice");
+
+?>
